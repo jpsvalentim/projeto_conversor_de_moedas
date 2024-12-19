61133d3c4970dac7ef00d1d930cc235b6f98c2e1
Valentim <j.pedro__@hotmail.com>
2024-12-19 16:10:20 -0300

    isso ai

--- projeto/main.py
+++ projeto/main.py
@@ -12,11 +12,14 @@ janela.geometry("500x500")
 #CRIAR OS BOTTONS, TEXTOS E OUTROS ELEMENTOS
 titulo = customtkinter.CTkLabel(janela, text = "Conversor de Moedas")#isso nao colaca direto na vizualização da janela, apenas em codigo
 texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem:")
+texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino:")
 
+campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=["USD","BRL","EUR","BTC"])
 
 #ORGANIZADOR DE ELEMENTOS NA TELA
-titulo.pack()
-texto_moeda_origem.pack()
+titulo.pack(padx=10, pady =10)
+
+texto_moeda_origem.pack(padx=10, pady =10)
 
 
 #LOOP 
