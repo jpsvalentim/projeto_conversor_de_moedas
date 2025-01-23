a4a769a2aa5755f97570dd3acefb9d4fe2c61ab2
Valentim <j.pedro__@hotmail.com>
2025-01-23 13:51:12 -0300

    nova paasta

--- projeto/main.py
+++ projeto/main.py
@@ -10,7 +10,7 @@ janela = customtkinter.CTk()
 janela.geometry("500x500")
 
 #CRIAR OS BOTTONS, TEXTOS E OUTROS ELEMENTOS
-titulo = customtkinter.CTkLabel(janela, text = "Conversor de Moedas")#isso nao colaca direto na vizualização da janela, apenas em codigo
+titulo = customtkinter.CTkLabel(janela, text = "CONVERSOR DE MOEDAS", font=("Arial",30))#isso nao colaca direto na vizualização da janela, apenas em codigo
 texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem:")
 texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino:")
 
@@ -22,12 +22,18 @@ botao_converter = customtkinter.CTkButton(janela,text="Converter", command=conve
 
 lista_moedas = customtkinter.CTkScrollableFrame(janela)
 
+moedas_disponiveis = ["USD é dolar amaericano","BRL é real brasileiro","BCT é bitcoin"]
+for moeda in moedas_disponiveis:
+    texto_moeda = customtkinter.CTkLabel(lista_moedas, text=moeda)
+    texto_moeda.pack()
+
+
 #ORGANIZADOR DE ELEMENTOS NA TELA
 titulo.pack(padx=10, pady =10)
 texto_moeda_origem.pack(padx=10, pady =10)
-campo_moeda_origem.pack(padx=10, pady=10)
+campo_moeda_origem.pack(padx=10)
 texto_moeda_destino.pack(padx =10, pady = 10)
-campo_moeda_destino.pack(padx=10, pady=10)
+campo_moeda_destino.pack(padx=10)
 botao_converter.pack(padx =10, pady=10)
 
 lista_moedas.pack(padx =10, pady=10)
