17d484b4ce39fe1629e606c08bd8069a89cfd153
Valentim <j.pedro__@hotmail.com>
2025-01-23 16:29:55 -0300

    mais um

--- projeto/main.py
+++ projeto/main.py
@@ -1,5 +1,7 @@
 print("oi")
 import customtkinter
+from PEGAR_MOEDA import nomes_moedas, conversoes_disponiveis;
+
 
 #CRIAR E CONFIGURAR JANELA
 customtkinter.set_appearance_mode("dark")
@@ -9,22 +11,30 @@ customtkinter.set_default_color_theme("dark-blue")
 janela = customtkinter.CTk()
 janela.geometry("500x500")
 
+dic_conversoes_disponiveis = conversoes_disponiveis()
+
 #CRIAR OS BOTTONS, TEXTOS E OUTROS ELEMENTOS
 titulo = customtkinter.CTkLabel(janela, text = "CONVERSOR DE MOEDAS", font=("Arial",30))#isso nao colaca direto na vizualização da janela, apenas em codigo
 texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de origem:")
 texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino:")
 
-campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=["USD","BRL","EUR","BTC"])
-campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values =["USD","BRL","EUR","BTC"])
+def carregar_moedas_destino(moeda_selecionada):
+    lista_moedas_destino = dic_conversoes_disponiveis[moeda_selecionada]
+    campo_moeda_destino.configure(values=lista_moedas_destino)
+
+campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values = list(dic_conversoes_disponiveis.keys()),command=carregar_moedas_destino)
+campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values =["Selecione uma moeda de origem"])
+
 def converter_moeda():
     print("converter moeda")
 botao_converter = customtkinter.CTkButton(janela,text="Converter", command=converter_moeda)
 
 lista_moedas = customtkinter.CTkScrollableFrame(janela)
 
-moedas_disponiveis = ["USD é dolar amaericano","BRL é real brasileiro","BCT é bitcoin"]
-for moeda in moedas_disponiveis:
-    texto_moeda = customtkinter.CTkLabel(lista_moedas, text=moeda)
+moedas_disponiveis = nomes_moedas()
+for codigo_moeda in moedas_disponiveis:
+    nome_moeda = moedas_disponiveis[codigo_moeda]
+    texto_moeda = customtkinter.CTkLabel(lista_moedas, text=f"{codigo_moeda}: {nome_moeda}")
     texto_moeda.pack()
 
 
