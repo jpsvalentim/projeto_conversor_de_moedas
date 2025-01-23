18900c4227f8ce7fbe33b64a73e0f350cec53769
Valentim <j.pedro__@hotmail.com>
2025-01-23 10:50:17 -0300

    mai um

--- projeto/main.py
+++ projeto/main.py
@@ -15,12 +15,22 @@ texto_moeda_origem = customtkinter.CTkLabel(janela, text="Selecione a moeda de o
 texto_moeda_destino = customtkinter.CTkLabel(janela, text="Selecione a moeda de destino:")
 
 campo_moeda_origem = customtkinter.CTkOptionMenu(janela, values=["USD","BRL","EUR","BTC"])
+campo_moeda_destino = customtkinter.CTkOptionMenu(janela, values =["USD","BRL","EUR","BTC"])
+def converter_moeda():
+    print("converter moeda")
+botao_converter = customtkinter.CTkButton(janela,text="Converter", command=converter_moeda)
+
+lista_moedas = customtkinter.CTkScrollableFrame(janela)
 
 #ORGANIZADOR DE ELEMENTOS NA TELA
 titulo.pack(padx=10, pady =10)
-
 texto_moeda_origem.pack(padx=10, pady =10)
+campo_moeda_origem.pack(padx=10, pady=10)
+texto_moeda_destino.pack(padx =10, pady = 10)
+campo_moeda_destino.pack(padx=10, pady=10)
+botao_converter.pack(padx =10, pady=10)
 
+lista_moedas.pack(padx =10, pady=10)
 
 #LOOP 
 janela.mainloop()
